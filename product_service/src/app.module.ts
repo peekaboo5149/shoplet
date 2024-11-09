9b023fcb42241e0c6684d0c5cf188ce8e0e9a416
Tanmay Kumar <ktanmay5149@gmail.com>
2024-11-10 02:01:55 +0530

    Prometheus func added

--- product_service/src/app.module.ts
+++ product_service/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { ApiModule } from './api/api.module';
+import { ApiModule } from './api/api.module'
+import { PrometheusModule } from '@willsoto/nestjs-prometheus'
+import { APP_INTERCEPTOR } from '@nestjs/core'
+import { LoggingInterceptor } from './logging.interceptor'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ApiModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrometheusModule.register(),
+    ApiModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
